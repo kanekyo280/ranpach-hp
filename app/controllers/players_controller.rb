d732ea1e1779e10685e49ac8b6b7be9629518813
kanekyo280 <kyosukekanemura109@gmail.com>
2024-08-27 21:49:21 +0900

    詳細編集削除機能実装完了

---
+++ app/controllers/players_controller.rb
@@ -0,0 +1,50 @@
+class PlayersController < ApplicationController
+  before_action :set_player, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @player = Player.all
+  end
+
+  def new
+    @player = Player.new
+  end
+
+  def create
+    @player = Player.new(player_params)
+    if @player.save
+      redirect_to players_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @player.update(player_params)
+      redirect_to player_path
+    else
+      render :edit,status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @player.destroy
+    redirect_to players_path
+  end
+
+  private
+  def player_params
+    params.require(:player).permit(:player_name, :player_kana, :position_id, :image)
+  end
+
+  def set_player
+    @player = Player.find(params[:id])
+  end
+end
